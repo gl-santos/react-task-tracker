e0d096e1687b9ced9d7e5e82c245fd66b5f81085
Gustavo Santos <glsantos@protonmail.com>
2025-12-05 20:51:09 -0300

    v4: Persisting data with server file

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
-
+const DB_FILE = path.join(__dirname, 'tasks.json');
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL }));
 
-let tasks = [
-  { id: 1, text: 'Sample task', done: false }
-];
+let tasks = [];
+if (fs.existsSync(DB_FILE)) {
+  tasks = JSON.parse(fs.readFileSync(DB_FILE, 'utf8'));
+}
+else {
+  fs.writeFileSync(DB_FILE, JSON.stringify(tasks));
+}
+
+const saveTasks = () => fs.writeFileSync(DB_FILE, JSON.stringify(tasks, null, 2));
 
 app.get('/', (req, res) => {
   res.json({ status: 'Backend alive', tasksCount: tasks.length });
@@ -26,6 +34,7 @@ app.post('/tasks', (req, res) => {
   if (!text) return res.status(400).json({ error: 'Text required' });
   const newTask = { id: Date.now(), text, done: false };
   tasks.push(newTask);
+  saveTasks();
   res.status(201).json(newTask);
 });
 
@@ -34,13 +43,15 @@ app.put('/tasks/:id', (req, res) => {
   const task = tasks.find(t => t.id === id);
   if (!task) return res.status(404).json({ error: 'Task not found' });
   task.done = !task.done;
+  saveTasks();
   res.json(task);
 });
 
 app.delete('/tasks/:id', (req, res) => {
   const id = parseInt(req.params.id);
   tasks = tasks.filter(t => t.id !== id);
-  res.status(204).send();  // No content = success
+  saveTasks();
+  res.status(204).send();
 });
 
 app.listen(PORT, () => {
